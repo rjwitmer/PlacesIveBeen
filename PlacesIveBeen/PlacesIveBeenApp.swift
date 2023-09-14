9d83daabf9c4d0efdbcc8996a20be1d79d7416ea
Bob Witmer <bwitmer@golden.net>
2023-09-14 15:32:07 -0400

    Initial Commit

---
+++ PlacesIveBeen/PlacesIveBeenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesIveBeenApp.swift
+//  PlacesIveBeen
+//
+//  Created by Bob Witmer on 2023-09-14.
+//
+
+import SwiftUI
+
+@main
+struct PlacesIveBeenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
